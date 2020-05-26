305739c039804cd5e490010d32f5028773a3bcbd
rahul_5409 <rahul.k.ravishankar@gmail.com>
2020-05-26 20:30:07 +0530

    Create Day26-ContiguousArray.cpp

---
+++ Week4/Day26-ContiguousArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+        
+        int n=nums.size();
+        map<int,int> m;
+        m[0]=-1;
+        int count=0;
+        int maxLen=0;
+        for(int i=0;i<n;i++)
+        {
+            count+=nums[i]==1?1:-1;
+            if(m.find(count)==m.end())
+                m[count]=i;
+            else
+                maxLen=max(maxLen,i-m[count]);
+        }
+        return maxLen;
+    }
+};
